ec01bbe8f8bec1ff92afe9abbb12c2e85d2842d2
SruthiRinky <132438733+SruthiRinky@users.noreply.github.com>
2023-05-04 15:20:42 +0530

    Update .py

--- .py
+++ .py
@@ -1,1 +1,20 @@
+class Dog:
+  #class variable
+  animal='Dog'
+  #the init method or constructor
+  def __init__(self,breed,colour):
+    #Instance Variable
+    self.breed=breed
+    self.colour=colour
+#objects of dog class
+Rodger=Dog("Pug","brown")
+Buzo=Dog("Bulldog","black")
+print('Rodger details:')
+print('Rodger is a',Rodger.animal)
+print('breed:',Rodger.breed)
+print('colour:',Rodger.colour)
 
+print('\nBuzo details:')
+print('Buzo is a',Buzo.animal)
+print('Breed:',Buzo.breed)
+print('colour:',Buzo.colour)
